7ee9777cb59bc8d4ecb1ecd885237ebe3b4a3cc6
MD Jahid Hasan <jahid60012@gmail.com>
2020-08-11 19:35:00 +0600

    Minor changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', ()=> {
-    if(localStorage.getItem('currentLevel')) {
-        selectLevel(localStorage.getItem('currentLevel'));
-    } else {
-        selectLevel();
-    }
-    showHighScore();
+    // if(localStorage.getItem('currentLevel')) {
+    //     selectLevel(localStorage.getItem('currentLevel'));
+    // } else {
+    //     selectLevel();
+    // }
+    // showHighScore();
+    init();
 });
-
 
 let time = 0;
 let score = 0;
@@ -16,6 +16,7 @@ let levelDivider;
 let timer = undefined;
 let isCountdownStart = false;
 let selectedLevel = undefined;
+let firstIteration = true;
 
 const wordInput = document.querySelector('#word-input');
 const currentWord = document.querySelector('#current-word');
@@ -31,8 +32,8 @@ const easyMode = document.querySelector("#easy");
 const mediumMode = document.querySelector("#medium");
 const hardMode = document.querySelector("#hard");
 
-wordMode.addEventListener('click', ()=> {selectMode('word');})
-sentenceMode.addEventListener('click', ()=> {selectMode('sentence');})
+wordMode.addEventListener('click', ()=> { selectMode('word'); })
+sentenceMode.addEventListener('click', ()=> { selectMode('sentence'); })
 tryAgain.addEventListener('click', () => {
     location.reload();
     return false;
@@ -42,33 +43,51 @@ easyMode.addEventListener('click', () => selectLevel('Easy'));
 mediumMode.addEventListener('click', () => selectLevel('Medium'));
 hardMode.addEventListener('click', () => selectLevel('Hard'));
 
+var i = 0;
 function init () {
-    if (localStorage.getItem('isInSentenceMode') === 'true') {
-        sentenceMode.classList.add('active');
-        isInSentenceMode = true;
+    console.log(i++);
+    // if (localStorage.getItem('isInSentenceMode') === 'true') {
+    //     sentenceMode.classList.add('active');
+    //     isInSentenceMode = true;
+    // }
+    // else {
+    //     wordMode.classList.add('active');
+    //     isInSentenceMode = false;
+    // }
+    // if(localStorage.getItem('currentLevel')) {
+    //     selectLevel(localStorage.getItem('currentLevel'));
+    // } else {
+    //
+    // }
+    if(firstIteration) {
+        selectLevel(null);
+        selectMode(null);
+        showHighScore();
     }
-    else {
-        wordMode.classList.add('active');
-        isInSentenceMode = false;
-    }
-    showHighScore();
 
     score = 0;
     scoreDisplay.innerHTML = 0;
     timeDisplay.innerText = '..';
     time = null;
-    if(isInSentenceMode) {
-        if(localStorage.getItem('highScoreInSentenceMode'))
-            highScore.innerText = localStorage.getItem('highScoreInSentenceMode');
-        else if(localStorage.getItem('highScoreInWordMode')) {
-            highScore.innerText = localStorage.getItem('highScoreInWordMode');
-        }
-    }
+    // if(isInSentenceMode) {
+    //     if(localStorage.getItem('highScoreInSentenceMode'))
+    //         highScore.innerText = localStorage.getItem('highScoreInSentenceMode');
+    //     else if(localStorage.getItem('highScoreInWordMode')) {
+    //         highScore.innerText = localStorage.getItem('highScoreInWordMode');
+    //     }
+    // }
     showWord();
     wordInput.addEventListener('input', startMatch);
+    firstIteration = false;
+    console.log('init');
 }
 
-function selectLevel(level = 'Easy') {
+function selectLevel(level) {
+    if(level === null) {
+        if(localStorage.getItem('currentLevel')) level = localStorage.getItem('currentLevel');
+        else level = 'Easy';
+    }
+
     if(level === 'Easy') levelDivider = 2;
     else if(level === 'Medium') levelDivider = 2.5;
     else levelDivider = 3;
@@ -81,22 +100,33 @@ function selectLevel(level = 'Easy') {
     isStartTyping = false;
     selectedLevel = level;
 
-    init();
+    if(!firstIteration) init();
+    console.log('selectLevel-'+level)
 }
 
 function selectMode(mode) {
+    if(mode === null) {
+        if(localStorage.getItem('isInSentenceMode')) {
+            if(localStorage.getItem('isInSentenceMode')) mode = 'sentence';
+            else mode = 'word';
+        }
+        mode = 'word';
+    }
     mode === 'word' ? wordMode.classList.add('active') : wordMode.classList.remove('active');
     mode === 'word' ? localStorage.setItem('isInSentenceMode', 'false') : localStorage.setItem('isInSentenceMode', 'true');
     mode === 'sentence' ? sentenceMode.classList.add('active') : sentenceMode.classList.remove('active');
+    mode === 'sentence' ? isInSentenceMode = true : isInSentenceMode = false;
 
     if(timer) clearInterval(timer);
     isStartTyping = false;
+    // isInSentenceMode = true;
 
-    init();
+    if(!firstIteration) init();
+    console.log('selectMode-'  + isInSentenceMode + '-' + mode);
 }
 
 async function showWord() {
-    if(isInSentenceMode) fetchUrl = 'http://api.quotable.io/random';
+    if(isInSentenceMode) fetchUrl = 'https://api.quotable.io/random';
     else fetchUrl = 'https://random-word-api.herokuapp.com/word?number=1';
     let word = await fetch(fetchUrl)
         .then(res => res.json())
@@ -111,6 +141,7 @@ async function showWord() {
         characterSpan.innerText = character;
         currentWord.appendChild(characterSpan);
     })
+    console.log('showWord')
 }
 
 function countdown() {
@@ -135,12 +166,12 @@ function countdown() {
             if(timer) clearInterval(timer);
         }
     }
+    console.log('countdown')
 }
 
 function startMatch() {
     isStartTyping = true;
     if(!isCountdownStart) {
-        // if(timer) clearInterval(timer);
         timer = setInterval(countdown, 1000);
     }
     isCountdownStart = true;
@@ -150,11 +181,8 @@ function startMatch() {
         wordInput.value = '';
         score++;
     }
-    if(score == -1) {
-        localStorage.setItem('errororor', 'oisahfoerhfoiuerhfiouerhfiuedhf');
-        scoreDisplay.innerHTML = 0;
-    } else
-        scoreDisplay.innerHTML = score;
+    scoreDisplay.innerHTML = score;
+    console.log('startMatch')
 }
 
 function matchWords() {
@@ -189,10 +217,12 @@ function matchWords() {
         message.innerHTML = '';
         return false;
     }
+    console.log('matchWord')
 }
 
 function showHighScore() {
     let mode = 'highScore' + (isInSentenceMode ? 'Sentence' : 'Word') + selectedLevel;
     if(localStorage.getItem(mode)) highScore.innerText = localStorage.getItem(mode);
     else highScore.innerText = '..';
+    console.log('showHighScore');
 }
